d1f95f6fbefb8bddcfee060b4cdfe16be71d6568
PhamHongKhoi <khoiphph19840@fpt.edu.vn>
2023-10-12 22:42:56 +0700

    address

---
+++ src/main/java/com/backend/entity/Address.java
@@ -0,0 +1,58 @@
+package com.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "adress")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "specific_address")
+    private String specificAddress;
+
+    @Column(name = "ward")
+    private Integer ward;
+
+    @Column(name = "district")
+    private Integer district;
+
+    @Column(name = "province")
+    private Integer province;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "default_address")
+    private String defaultAddress;
+
+}
