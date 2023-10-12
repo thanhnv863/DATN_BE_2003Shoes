a94d4b401bafbd40f702d2eaa51b3c48b3b2dc23
Trương Mạnh Quang <truongmanhquang2k3@gmail.com>
2023-10-12 11:10:09 +0700

    changeURL

--- src/main/java/com/backend/controller/VoucherOrderController.java
+++ src/main/java/com/backend/controller/VoucherOrderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("/api/v1/admin/voucher-order")
 public class VoucherOrderController {
 
     @Autowired
