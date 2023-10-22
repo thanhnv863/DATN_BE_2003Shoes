aeb42058a83c2ce088abdbb2a3b80c6a0761a18b
manh <manhndph20068@fpt.edu.vn>
2023-10-22 09:58:23 +0700

    crud cart, cart detail

---
+++ src/main/java/com/backend/dto/response/CartDetailResponse.java
@@ -0,0 +1,19 @@
+package com.backend.dto.response;
+
+import com.backend.dto.response.shoedetail.ResultItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartDetailResponse {
+    private Long id;
+    private Integer quantity;
+    private ResultItem detail;
+}
--- src/main/java/com/backend/service/impl/AccountServiceImpl.java
+++ src/main/java/com/backend/service/impl/AccountServiceImpl.java
@@ -4,18 +4,25 @@ import com.backend.ServiceResult;
 import com.backend.config.AppConstant;
 import com.backend.dto.request.RegisterRequest;
 import com.backend.dto.response.RegisterResponse;
+import com.backend.entity.Cart;
 import com.backend.entity.Role;
 import com.backend.entity.Account;
 import com.backend.repository.AccountRepository;
+import com.backend.repository.CartRepository;
 import com.backend.service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements IAccountService {
+
+    @Autowired
+    private CartRepository cartRepository;
 
     @Autowired
     private AccountRepository accountRepository;
@@ -30,7 +37,15 @@ public class AccountServiceImpl implements IAccountService {
         account.setEmail(registerRequest.getEmail());
         account.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         account.setRole(Role.builder().id(registerRequest.getIdRole()).build());
-        accountRepository.save(account);
+        account = accountRepository.save(account);
+        Cart cart = new Cart();
+        Calendar calendar = Calendar.getInstance();
+        Date now = calendar.getTime();
+        cart.setAccount(account);
+        cart.setCreatedAt(now);
+        cart.setUpdatedAt(now);
+        cart.setStatus(1);
+        cartRepository.save(cart);
         RegisterResponse registerResponse = new RegisterResponse();
         return new ServiceResult<>(AppConstant.SUCCESS,
                 "Registered Successfully",
