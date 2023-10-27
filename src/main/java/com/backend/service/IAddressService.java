2caf71d6e78abec9eae41ee59d1ce5880af0b67a
PhamHongKhoi <khoiphph19840@fpt.edu.vn>
2023-10-27 16:18:36 +0700

    "address searchname"

--- src/main/java/com/backend/service/IAddressService.java
+++ src/main/java/com/backend/service/IAddressService.java
@@ -3,11 +3,10 @@ package com.backend.service;
 import com.backend.ServiceResult;
 import com.backend.dto.request.AddressRequest;
 import com.backend.dto.response.AddressResponse;
-import com.backend.dto.response.VoucherOrderResponse;
-import com.backend.dto.response.shoedetail.DataPaginate;
 import com.backend.entity.Address;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IAddressService {
     ServiceResult<AddressResponse> addAddress(AddressRequest addressRequest);
@@ -23,4 +22,8 @@ public interface IAddressService {
     ServiceResult<List<AddressResponse>> getAllAddress();
 
     ServiceResult<Address> deleteAddress(AddressRequest addressRequest);
+
+    ServiceResult<List<Address>> searchNameClient(String name);
+
+
 }
