5e14337799bd722c6e5af647824abcab1c64c141
PhamHongKhoi <khoiphph19840@fpt.edu.vn>
2023-11-07 22:04:25 +0700

    "satistical"

---
+++ src/main/java/com/backend/controller/StatisticalController.java
@@ -0,0 +1,44 @@
+package com.backend.controller;
+
+import com.backend.dto.statistical.HoaDonHuy;
+import com.backend.service.IStatistical;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/statistical")
+public class StatisticalController {
+
+    @Autowired
+    private IStatistical iStatistical;
+
+//    public ResponseEntity<?> addressName(@RequestParam("name") String name){
+//        return ResponseEntity.ok(iAddressService.searchNameClient(name));
+//    }
+
+    @GetMapping("/hoadonhuy")
+    public ResponseEntity<?> getHoaDonHuy(@RequestParam("ngayTao") Integer ngayTao,
+                                          @RequestParam("trangThai") Integer trangThai){
+        return ResponseEntity.ok(iStatistical.findHoaDonHuy(ngayTao,trangThai));
+    }
+
+    @GetMapping("/sanphambanchaytrongnam")
+    public ResponseEntity<?> getHangHoa(@RequestParam("nam") Integer nam){
+        return ResponseEntity.ok(iStatistical.thongKeSanPhamBanChayTrongNam(nam));
+    }
+
+    @GetMapping("/doanhthutrongngay")
+    public ResponseEntity<?> getDoanhThuTrongNgay(@RequestParam("ngay") Integer ngay){
+        return ResponseEntity.ok(iStatistical.doanhThuTrongNgay(ngay));
+    }
+
+    @GetMapping("/doanhthutrongthang")
+    public ResponseEntity<?> getDoanhThuTrongThang(@RequestParam("thang") Integer thang){
+        return ResponseEntity.ok(iStatistical.doanhThuTrongThang(thang));
+    }
+}
---
+++ src/main/java/com/backend/repository/StatisticalRepository.java
@@ -0,0 +1,68 @@
+package com.backend.repository;
+
+import com.backend.entity.Address;
+import com.backend.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StatisticalRepository extends JpaRepository<Order,Long> {
+
+//    @Query("select a from Address a where " +
+//            " a.name like concat('%', :name, '%')")
+//    List<Address> searchNameClient(String name);
+
+    @Query(value = "select \n" +
+            "\tday(mo.create_date) as Ngay,\n" +
+            "    COUNT(*) as soHoaDonHuy\n" +
+            " from my_order mo \n" +
+            "\t\twhere day(mo.create_date) = :ngayTao and status = :trangThai\n" +
+            "        GROUP BY day(mo.create_date) \n" +
+            "        ORDER BY Ngay",nativeQuery = true)
+    List<Object[]> findByHoaDonHuy(Integer ngayTao, Integer trangThai);
+
+    @Query(value = "select \n" +
+            "\ts.name as 'Tên sản phẩm', \n" +
+            "    CASE WHEN month(oh.created_time) = 1 THEN sum(od.quantity) ELSE 0 END AS 'tháng 1',\n" +
+            "    CASE WHEN month(oh.created_time) = 2 THEN sum(od.quantity) ELSE 0 END AS 'tháng 2',\n" +
+            "    CASE WHEN month(oh.created_time) = 3 THEN sum(od.quantity) ELSE 0 END AS 'tháng 3',\n" +
+            "    CASE WHEN month(oh.created_time) = 4 THEN sum(od.quantity) ELSE 0 END AS 'tháng 4',\n" +
+            "    CASE WHEN month(oh.created_time) = 5 THEN sum(od.quantity) ELSE 0 END AS 'tháng 5',\n" +
+            "    CASE WHEN month(oh.created_time) = 6 THEN sum(od.quantity) ELSE 0 END AS 'tháng 6',\n" +
+            "    CASE WHEN month(oh.created_time) = 7 THEN sum(od.quantity) ELSE 0 END AS 'tháng 7',\n" +
+            "    CASE WHEN month(oh.created_time) = 8 THEN sum(od.quantity) ELSE 0 END AS 'tháng 8',\n" +
+            "    CASE WHEN month(oh.created_time) = 9 THEN sum(od.quantity) ELSE 0 END AS 'tháng 9',\n" +
+            "    CASE WHEN month(oh.created_time) = 10 THEN sum(od.quantity) ELSE 0 END AS 'tháng 10',\n" +
+            "    CASE WHEN month(oh.created_time) = 11 THEN sum(od.quantity) ELSE 0 END AS 'tháng 11',\n" +
+            "    CASE WHEN month(oh.created_time) = 12 THEN sum(od.quantity) ELSE 0 END AS 'tháng 12',\n" +
+            "\tcoalesce(sum(od.quantity), 0) as 'Tổng số lượng'\n" +
+            " from order_detail od \n" +
+            "\t\t\t join shoe_detail sd on od.shoe_detail_id = sd.id\n" +
+            "             join order_history oh on od.order_id = oh.order_id\n" +
+            "             join shoe s on sd.shoe_id = s.id\n" +
+            "            where YEAR(oh.created_time) = :nam\t\n" +
+            "            group by s.name, month(oh.created_time)\n" +
+            "            order by s.name",nativeQuery = true)
+    List<Object[]> findByHangHoaBanChayTrongNam(Integer nam);
+
+    @Query(value = "select \n" +
+            "\tday(mo.create_date) as Ngay,\n" +
+            "    sum(mo.total_money) as tongTien\n" +
+            " from my_order mo \n" +
+            "\t\twhere day(mo.create_date) = :ngay\n" +
+            "        GROUP BY day(mo.create_date)\n" +
+            "        ORDER BY Ngay",nativeQuery = true)
+    List<Object[]> doanhThuTrongNgay(Integer ngay);
+
+    @Query(value = "select \n" +
+            "\tmonth(mo.create_date) as Thang,\n" +
+            "    sum(mo.total_money) as tongTien\n" +
+            " from my_order mo \n" +
+            "\t\twhere month(mo.create_date) = :thang\n" +
+            "        GROUP BY month(mo.create_date)\n" +
+            "        ORDER BY Thang", nativeQuery = true)
+    List<Object[]> doanhThuTrongThang(Integer thang);
+}
