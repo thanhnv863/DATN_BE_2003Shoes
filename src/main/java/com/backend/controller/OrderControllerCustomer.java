593958ce5c30f6d3442b0811116509d2c3921532
thanh862003hhbg <thanh862003hhbg@gmail.com>
2023-11-09 21:26:59 +0700

    customer Order

---
+++ src/main/java/com/backend/controller/OrderControllerCustomer.java
@@ -0,0 +1,28 @@
+package com.backend.controller;
+
+import com.backend.dto.request.orderCustomer.SearchOrderCutomerRequest;
+import com.backend.entity.Order;
+import com.backend.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer/order")
+public class OrderControllerCustomer {
+    @Autowired
+    private IOrderService iOrderService;
+
+    @PostMapping("/get-all/customer")
+    public ResponseEntity<?> getAllOrder(@RequestBody SearchOrderCutomerRequest searchOrderCutomerRequest) {
+        List<Order> listOrderCustomer = iOrderService.listAllByCustomer(searchOrderCutomerRequest);
+        return ResponseEntity.ok().body(listOrderCustomer);
+    }
+
+
+}
