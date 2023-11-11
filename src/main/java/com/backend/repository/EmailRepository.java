3dea04027f62764e8866ac6154e935c3824f6ee8
thanhnv863 <thanh862003hhbg@gmail.com>
2023-11-11 10:09:42 +0700

    Thêm annotation

--- src/main/java/com/backend/repository/EmailRepository.java
+++ src/main/java/com/backend/repository/EmailRepository.java
@@ -3,9 +3,10 @@ package com.backend.repository;
 import com.backend.entity.EmailTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface EmailRepository extends JpaRepository<EmailTemplate, Long> {
     @Query(value = "SELECT * FROM email_template where email_template.mail_type = ?1", nativeQuery = true)
     Optional<EmailTemplate> checkSendMail(Integer type);
